55a2a84029ff4000640316ec6b607544a88dc8a1
jhwang37 <jhwang37@gmail.com>
2021-10-24 01:56:20 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,132 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "http://github/jhwang37"
+        },
+        {
+            "icon": "dribbble",
+            "url": "http://dribbble"
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype/live:.cid.3788bcf52f8cf303"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "www.linkedin.com/in/james-hwang37"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "ae30ed65-7cb2-44b9-9636-ca51bd2527a4",
+            "title": "Programming Instructor | C3 Cyberclub",
+            "timeline": "August 2019 - Present",
+            "description": "• Teaching Elementary/Middle school children programming and game design through various programs;\nPython, MCreator, Scratch, LegoEV3, and Kodu.\n",
+            "responsibilities": [
+                " Creating lesson plans for the classroom on the various programs",
+                "Create fun and interesting ways to make programming simple to understand while also accessible. "
+            ]
+        },
+        {
+            "__key": "f6882b43-aaa4-4412-939b-b63979e6f06b",
+            "title": "Java Web Developer Bootcamp",
+            "timeline": "Dec 2019 - May 2020",
+            "responsibilities": [
+                "OOP concepts, Collections, Database & Algorithms.",
+                "Spring framework, databases(SQL, SQLite, MongoDB), Spring security & authentication. ",
+                "HTML, CSS, Bootstrap",
+                "Git & SVN"
+            ],
+            "description": "Learn extensively about core Java and the Spring framework."
+        },
+        {
+            "__key": "d335d8b8-34bc-4093-b8fb-2ea80dd328a0",
+            "title": "Dental Assistant | Esthetic and General Dentistry Rockville",
+            "timeline": "Jan 2017 - July 2019",
+            "description": "Work chairside with the attending Dentist during specialized procedures, ensuring all equipment is on hand and\nthe patient is comfortable during the procedure.\n",
+            "responsibilities": [
+                "Expose and process dental X-rays; maintain patient records and lab equipment; sterilize instruments; assist\noffice managers with patient scheduling, billing and insurance claims; and educate patients on postoperative and\ngeneral oral health care.",
+                "Assist patients by providing answers to questions about specific treatments.\n"
+            ]
+        },
+        {
+            "__key": "792327ea-eaf3-4713-a0fa-376b73c99a22",
+            "title": "Team Leader | AccessAbility MedCare",
+            "timeline": "May 2012 - November 2017",
+            "description": "• Teaching Elementary/Middle school children programming and game design through various programs;\nPython, MCreator, Scratch, LegoEV3, and Kodu.\n",
+            "responsibilities": [
+                " Developed and implemented custom cognitive, functional and behavioral curriculum to support students with Autism. ",
+                "Assisted youth and adults with autism, seizure disorders, quadriplegia\n",
+                "Oversaw technicians on a daily basis to ensure consistent quality in service and practice"
+            ]
+        }
+    ],
+    "name": "James Hwang",
+    "header": "Hi, I'm James Hwang",
+    "cta": {
+        "label": "Get my resume",
+        "url": "resume.pdf"
+    },
+    "testimonials": [],
+    "title": "",
+    "projects": [
+        {
+            "__key": "472ad243-fd79-4d91-b7a2-06228d4dc4c3",
+            "name": "Montery Bay Aquarium ",
+            "description": "Created using Java and the Spring framework. Website was created as a non-profit organization project\n",
+            "tags": [
+                "Java",
+                "Spring",
+                "REST",
+                "API",
+                "Database"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "aquarium.png",
+            "url": "https://montery-bay-aquarium.herokuapp.com/"
+        },
+        {
+            "__key": "76c50192-b622-4c1a-ad27-cd4b4adb6b20",
+            "name": "E-Commerce",
+            "description": "Created using Java and the Spring framework.\nUsing relational databases in order to create a cart system that emulates buying, updating, and removing from the cart. ",
+            "tags": [
+                "Java",
+                "Spring",
+                "Relational Database",
+                "HTML",
+                "CSS",
+                "REST"
+            ],
+            "img": "E-Commerce.png",
+            "url": "https://e-commerce-coffee.herokuapp.com/"
+        },
+        {
+            "__key": "a40ca7d5-61ae-4b28-b445-480bc6a0f750",
+            "name": "Car E-Commerce",
+            "description": "Created using Java and the Spring framework. Using relational databases in order to use CRUD functionality. \n",
+            "tags": [
+                "Java",
+                "Spring",
+                "REST",
+                "API",
+                "Database",
+                "CRUD"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "CarShopping.png",
+            "url": "https://carshopping.herokuapp.com/"
+        }
+    ],
+    "email": "jhwang37@gmail.com",
+    "description": "I'm a college senior currently taking up Computer Science at University of Maryland Global Campus. My expected graduation date is June 2022. I currently work as an Instructor, teaching kids the basics of programming. I hope to not only become a valuable member of a team one day, but also bring joy in not only my team members lives, but to others through technology. \n\n \n\n",
+    "footerTitle": "Software Developer"
+}
